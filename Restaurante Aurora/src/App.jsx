db67ca63d733678fe550fce403cd2b537d32e0df
Leonardo Ian <leos13790@gmail.com>
2025-11-12 11:47:09 -0300

    feat: create section teacher in index.jsx

--- Restaurante Aurora/src/App.jsx
+++ Restaurante Aurora/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from "./components/Header"
 import Main from "./components/Main"
 import SectionDifferences from './components/SectionDifferences'
+import SectionTeacher from "./components/SectionTeacher"
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
       <Header/>
       <Main />
       <SectionDifferences/>
-      
+      <SectionTeacher/>
     </>
   )
 }
